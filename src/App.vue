05aa9d3dc1ea637f118c07837992510bce19c7e9
21DP4MGalk <14DPMGalkins@rvt.lv>
2023-10-12 18:57:56 +0300

    Partaisiju majaslapu pilniba

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+	<RouterLink to="/register">Register</RouterLink>
+	<RouterLink to="/register">Login</RouterLink>
+	<RouterLink to="/wood">The Woodworking Services</RouterLink>
+	<RouterLink to="/programs">The Software</RouterLink>
+	<RouterLink to="/code">The Open Source Code</RouterLink>
+	<a href="mailto:email@email.mail">Contact</a>
+      </nav>
+  </header>
+  <RouterView />
+</template>
+
+<style scoped>
+a {
+	margin-left: 5px;
+	margin-right: 3px;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,43 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+	path: '/register',
+	name: 'register',
+	component: () => import('../views/register.vue')
+    },
+    {
+    	path: '/wood',
+	name: 'wood',
+	component: () => import('../views/wood.vue')
+    },
+    {
+    	path: '/programs',
+	name: 'programs',
+	component: () => import('../views/programs.vue')
+    },
+    {
+    	path: '/code',
+	name: 'code',
+	component: () => import('../views/programs.vue')
+    },
+  ]
+})
+
+export default router
