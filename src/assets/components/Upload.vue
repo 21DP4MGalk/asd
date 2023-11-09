c502790fedb34486c5ecb09cc654333939b8f7f0
21DP4MGalk <14DPMGalkins@rvt.lv>
2023-11-09 03:04:07 +0200

    Mēģināju pievienot komponenti, nesanāca

---
+++ src/assets/components/Upload.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+const props = defineProps({
+	name:  String,
+	m_date: String,
+	description: String,
+	size: Number
+})
+
+</script>
+
+<template>
+	<div>
+	<p>Name: {{ upload.name }}
+Upload date: {{ upload.m_date }}
+Size: {{upload.size}} MB
+Short description: {{ upload.description }}
+	</p>
+	</div>
+</template>
+
+<style scoped>
+div{
+width: 70%;
+height: 25%;
+background-color:yellow;
+}
+</style>
--- src/views/programs.vue
+++ src/views/programs.vue
@@ -1,8 +1,21 @@
 <script setup>
+
+//import Upload from '../assets/components/Upload.vue'
+
 </script>
 <template>
+<div>
 	<h1>THIS IS OPEN SORES SOFTWARE</h1>
 	<h2>It's made of code and fail</h2>
 	<h2>Observe:</h2>
+</div>
 	<!--IDEA: Add a component so you can spam these to upload more stuff-->
+	<!--<Upload name="IDK" m_date="sire" description="test" size=1 />-->
 </template>
+
+<style scoped>
+div{
+	margin-left: 12px;
+}
+</style>
+
