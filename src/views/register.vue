228d5eaa699b54f2479f33d79cbafc5923f2c7bc
21DP4MGalk <103188871+21DP4MGalk@users.noreply.github.com>
2023-12-01 16:46:50 +0000

    Neatceros godīgi sakot

--- src/views/register.vue
+++ src/views/register.vue
@@ -1,4 +1,5 @@
 <script setup>
+//secret cheese is secret
 </script>
 
 <template>
@@ -6,13 +7,13 @@
 	<h1>Join our completely useless website</h1>
 	<h4>Every new account contributes to downfall of JS</h4>
 	</center>
-	<form>
+	<form action='/' method='POST'>
 		<label for='uname'>Username</label>
-		<input type='text' id='uname' required> <br>
+		<input name='uname' type='text' id='uname' required> <br>
 		<label for='email'>E-mail (optional)</label>
-		<input type='text' id='email'> <br>
+		<input name='email' type='text' id='email'> <br>
 		<label for='pwd'>Password</label>
-		<input type='password' id='pwd' minlength="10" required placeholder="At least 10 characters"> <br>
+		<input name='pwd' type='password' id='pwd' minlength="10" required placeholder="At least 10 characters"> <br>
 		<label for='pwd_confirm'>Confirm the password:</label>
 		<input type='password' id='pwd_confirm' minlength="10" required> <br>
 		<input type='submit' value='Register now' id="submit"> <br>
